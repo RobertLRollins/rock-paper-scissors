f24f6aa700d2afe86b89aa888d4d8f1cf187c755
RobertLRollins <robertlrollins@protonmail.com>
2021-12-11 20:25:04 -0500

    created .html and .js began to write pseudocode

---
+++ rock_paper_scissors.js
@@ -0,0 +1,12 @@
+/* computerPlay() function randomly return rock paper or scissors */
+
+/* playerSelection(make sure this input is not case-sensative) and computerSelection 
+parameters added to a function that plays a round of rock paper scissors and returns 
+string declaring a winner: "You Win! Rock beats Scissors" use console.log() to display
+the returned string. call this function playRound()*/
+
+/* Write a NEW function called game(). Use the previous function inside of this one 
+to play a 5 round game (call playRound 5 times) that keeps score and reports a winner 
+or loser at the end. Use console.log() to display each rounds result and winner at the 
+end. Use prompt to get user input. Feel free to re-work previous funtions/return values
+and to create new "helper funtions*/

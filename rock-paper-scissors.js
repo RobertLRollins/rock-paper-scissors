e0277b786953bce516e5c5378d69561c50368ba5
Robert L Rollins <robertlrollins@protonmail.com>
2023-11-24 14:38:50 -0500

    Add pseudocode to README.md, Add functions getComputerChoice and playRound

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+function getComputerChoice(){
+    let choices = [
+        "Rock",
+        "Paper",
+        "Scissors"
+    ];
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "Scissors" 
+    || playerSelection === "Scissors" && computerSelection === "Paper"
+    || playerSelection === "Paper" && computerSelection === "Rock") {
+            return "You Win! " + playerSelection + " beats " + computerSelection
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock" 
+    || playerSelection === "Paper" && computerSelection === "Scissors"
+    || playerSelection === "Rock" && computerSelection === "Paper") {
+        return "You Lose! " + computerSelection + " beats " + playerSelection
+    } else { return "It's a tie. You both picked " + computerSelection}
+}
+
+    const playerSelection = "Rock";
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
